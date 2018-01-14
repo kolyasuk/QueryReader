d7641685c7129342530c1a4c7c68a6c4e4668150
Mykola Dzioba <dzioba99@gmail.com>
2018-01-14 14:25:39 +0200

    Added writing result to file, modify reading from file

--- src/Main/MainApp.java
+++ src/Main/MainApp.java
@@ -1,83 +1,100 @@
 package Main;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class MainApp {
-	private static int fileReaderIndex = 0;
+	private static final String JDBC_DRIVER = "org.sqlite.JDBC";
+	private static String URL = "jdbc:sqlite:";
 	static Connection con = null;
 	static Statement stmt = null;
 	static ResultSet res = null;
+	static StringBuilder resultB;
 
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\kolyasuk\\Desktop\\Query.txt");
-		executeQuery(getQuery(file));
-		System.out.println("----------------------------");
-		executeQuery(getQuery(file));
+		File file = new File("C:/Users/kolyasuk/Desktop/Query.txt");
+		readQuery(file);
+		System.out.println(resultB.toString());
+		writeResult();
+
+	}
+
+	private static void readQuery(File file) {
+		try {
+			StringBuilder strb = new StringBuilder();
+			Scanner scan = new Scanner(file);
+			if (!scan.hasNextLine()) {
+				System.out.println("Файл Пуст!");
+				return;
+			}
+
+			String dbPath = scan.nextLine();
+
+			if (!new File(dbPath).exists()) {
+				System.out.println("DataBase file not exist");
+				return;
+			}
+
+			URL += dbPath;
+			while (scan.hasNextLine()) {
+				strb.append(scan.nextLine());
+			}
+
+			String[] query = strb.toString().split(";");
+			resultB = new StringBuilder();
+
+			for (int i = 0; i < query.length; i++) {
+				resultB.append("Запрос " + (i + 1) + ":" + "\n" + query[i] + "\n");
+				resultB.append("Результат:" + "\n");
+				executeQuery(query[i]);
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Query file not exist");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	private static void executeQuery(String query) {
 		try {
-			Driver driver = (Driver) Class.forName("org.sqlite.JDBC").newInstance();
+			Driver driver = (Driver) Class.forName(JDBC_DRIVER).newInstance();
 			DriverManager.deregisterDriver(driver);
-			
-			String url = "jdbc:sqlite:C:\\Users\\kolyasuk\\Desktop\\CarShop_lesson10.db";
-			
-			con = DriverManager.getConnection(url);
+
+			con = DriverManager.getConnection(URL);
 			stmt = con.createStatement();
-			
 			res = stmt.executeQuery(query);
-			while(res.next()) {
-				System.out.println(res.getString("name_en")+" - "+res.getString("name_ru"));
+
+			while (res.next()) {
+				resultB.append(res.getString("name_en") + " - " + res.getString("name_ru") + "\n");
 			}
-			
+			resultB.append("\n");
+
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Query incorrect");
 		}
 	}
-	
-	private static String getQuery(File file) {
-		String query = "";
+
+	private static void writeResult() {
+		File file = new File("C:/Users/kolyasuk/Desktop/result.txt");
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String tempQuery = reader.readLine();
-			
-			if (tempQuery==null) return null;
-			
-			else {
-				for (int i = fileReaderIndex; i < tempQuery.length(); i++) {
-					String temp = "" + tempQuery.charAt(i);
-					fileReaderIndex = i + 1;
-					
-					if (temp.equals(";")) break;
-					
-					query += tempQuery.charAt(i) + "";
-				}
-				return query;
-			}
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			FileWriter fw = new FileWriter(file);
+			fw.write(resultB.toString());
+			fw.flush();
+			fw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		return null;
 	}
 }
